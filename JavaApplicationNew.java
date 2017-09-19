ff53aef8c9d551f2a3ad3a6aba87f8de5ba975fc
Valeria Soricetti <31908760+vsole@users.noreply.github.com>
2017-09-19 16:37:00 -0300

    parsear

---
+++ JavaApplicationNew.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplicationnew;
+import java.util.Scanner;
+
+/**
+ *
+ * @author alumno
+ */
+public class JavaApplicationNew {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        String datoAux;
+        String nombre;
+        Integer cantidad;
+        Double precio;
+        Double precioFinal;
+        
+        Scanner miescanner= new Scanner(System.in);
+        
+        System.out.println("Ingrese cantidad del producto::");
+        datoAux=miescanner.next();
+        cantidad= Integer.parseInt(datoAux);
+        
+        System.out.println("Ingrese el precio del producto:");
+        datoAux=miescanner.next();
+        cantidad= Integer.parseInt(datoAux);// solo funciona si es un numero la palabra por ej: "22" , no "veinte"
+        
+         precioFinal=
+        
+        
+     
+        
+        
+        
+       System.out.println("Ingrese Dato 3:");
+        datoAux=miescanner.next();
+        
+        
+        
+        
+        
+        
+    }
+    
+}
