0c5a5b64cf1e4d6ea416c99c87afb033672547bb
Valeria Soricetti <31908760+vsole@users.noreply.github.com>
2017-09-19 18:00:01 -0300

    ejercicios para mañana

---
+++ Ejercicio15.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio.pkg15;
+
+/**
+ *
+ * @author alumno
+ */
+public class Ejercicio15 {
+
+    /**
+     * @param args the command line arguments
+     * 15
+     * se ingresa un importe, si supera los 100 pesosse le suma un 23 %.
+     * si es menor a 100 pesos, se le descuenta el 20%
+     * 16
+     * se ingresa el importe de un pasaje de avion y el mes de viaje,
+     * si es enero, se le descuenta un 10% por temporada baja.
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
